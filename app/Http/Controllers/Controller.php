a586973cf902e68df9c3db05274af63926f70b50
phyomin-coder <phyominkhant.metatech@gmail.com>
2025-04-29 16:17:41 +0630

    fix: controller with swagger api

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,34 @@
 
 namespace App\Http\Controllers;
 
-abstract class Controller
+/**
+ * @OA\Info(
+ *     title="Booking Swagger API",
+ *     version="1.0.0",
+ *     @OA\Contact(
+ *         email="admin@example.com"
+ *     ),
+ *     @OA\License(
+ *         name="Apache 2.0",
+ *         url="http://www.apache.org/licenses/LICENSE-2.0.html"
+ *     )
+ * )
+ * @OA\Tag(
+ *     name="Bookings",
+ *     description="Booking Pages",
+ * )
+ * @OA\Server(
+ *     description="Laravel Swagger API server",
+ *     url="http://localhost/api"
+ * )
+ * @OA\SecurityScheme(
+ *     type="apiKey",
+ *     in="header",
+ *     name="X-APP-ID",
+ *     securityScheme="X-APP-ID"
+ * )
+ */
+
+class Controller extends \App\Api\Foundation\Routing\Controller
 {
-    //
 }
